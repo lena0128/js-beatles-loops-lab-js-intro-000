3db503a3c815e890f0ecd2190b0833ecb40996a7
Lena Hou <lenahou2013@gmail.com>
2020-12-13 01:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+var musicians = [];
+var instruments = [];
+
+function theBeatlesPlay(musicians, instruments) {
+ var myList = [];
+  for(let i = 0; i < musicians.length; i++) {
+    var newArray = `${musicians[i]} plays ${instruments[i]}`;
+    myList.push(newArray[i]);
+  }
+  return myList;
+}
